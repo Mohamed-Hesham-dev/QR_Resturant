359412625f9709d2be72a3df72151393ffadc7b5
Mohamed Hesham <mohamed.hesham@innovito.com>
2023-08-15 18:51:09 +0300

    make header logo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,17 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-<<<<<<< HEAD
         $this->call(AdminSeeder::class);
         $this->call(ContactUsSettingSeeder::class);
         $this->call(AboutUsSettingSeeder::class);
       //  $this->call(ResturantContactUsSettingSeeder::class);
-=======
-        // $this->call(AdminSeeder::class);
-        // $this->call(ContactUsSettingSeeder::class);
-        // $this->call(AboutUsSettingSeeder::class);
-        $this->call(ResturantContactUsSettingSeeder::class);
->>>>>>> 3d700c8d0fecbc40a636229375dcdd8d439b30a6
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
